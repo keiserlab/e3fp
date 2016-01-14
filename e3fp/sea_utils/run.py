2adc82a0c147508b2eb30fda0deff2996c45eada
Seth Axen <seth.axen@gmail.com>
2016-01-14 10:46:00 -0800

    added docstrings and comments, updated to use new string formatting

--- e3fp/sea_utils/run.py
+++ e3fp/sea_utils/run.py
@@ -1,4 +1,4 @@
-"""Utilities for running SEA.
+"""Utilities for searching molecules against a SEA library.
 
 Author: Seth Axen
 E-mail: seth.axen@gmail.com
@@ -15,8 +15,27 @@ from seacore.util.library import TargetKey
 
 class SEASetSearcher(object):
 
+    """Wrapper for easily searching molecule sets against a SEA library."""
+
     def __init__(self, reference, log=False, mol_db=None, target_db=None,
                  wipe=True):
+        """Constructor.
+
+        Parameters
+        ----------
+        reference : str
+            Path to SEA library file.
+        log : bool, optional
+            Create log messages.
+        mol_db : str or None, optional
+            If path is provided, create a database of dynamically updated
+            molecule results.
+        target_db : str or None, optional
+            If path is provided, create a database of dynamically updated
+            target results.
+        wipe : bool, optional
+            Clear search results (including database) when opening library.
+        """
         self.log = log
         self.wipe = wipe
         self.core = SEASetCore()
@@ -31,6 +50,13 @@ class SEASetSearcher(object):
         self.open(reference)
 
     def open(self, reference):
+        """Open SEA library file. If already open, close and then reopen.
+
+        Parameters
+        ----------
+        reference : str
+            Path to library file.
+        """
         if self.wipe:
             self.clear_results()
         if self.core.library is not None:
@@ -45,6 +71,24 @@ class SEASetSearcher(object):
             logging.info("Library loaded.")
 
     def search(self, mol_name, fp_list, rerun=False):
+        """Search set of fingerprints for one molecule against library.
+
+        Parameters
+        ----------
+        mol_name : str
+            Name of molecule
+        fp_list : list of tuple
+            List of native tuples, each tuple containing native string and
+            fingerprint name.
+        rerun : bool, optional
+            If True, rerun search if result already exists.
+
+        Returns
+        -------
+        mol_results : dict
+            Dict of results. Keys are ``TargetKey``, values are a tuple of
+            e-value and maximum tanimoto coefficient.
+        """
         if not rerun and mol_name in self.set_results_dict:
             return self.set_results_dict[mol_name]
         try:
@@ -79,6 +123,18 @@ class SEASetSearcher(object):
         return self.set_results_dict.get(mol_name, {})
 
     def batch_search(self, molecules_lists_dict):
+        """Search multiple fingerprint sets against SEA library.
+
+        Parameters
+        ----------
+        molecules_lists_dict : dict
+            Dict matching molecule name to list of native_tuples.
+
+        Returns
+        -------
+        success : bool
+            True if batch search was successful, False if not.
+        """
         if self.core.library is None:
             logging.error(
                 "Reference library for search has not been defined.",
@@ -90,27 +146,73 @@ class SEASetSearcher(object):
         i = 0
         for result in search_iter:
             if (i + 1) % 100 == 0:
-                print("Searched %d molecules." % (i+1))
+                print("Searched {:d} molecules.".format(i+1))
             i += 1
         if self.log:
-            logging.info("Searched %d molecule sets against library." % (i+1))
+            logging.info(
+                "Searched {:d} molecule sets against library.".format(i+1))
         return True
 
     def mol_result(self, mol_name):
+        """Get results for molecule.
+
+        Parameters
+        ----------
+        mol_name : str
+            Name of molecule.
+
+        Returns
+        -------
+        mol_results : dict
+            Dict of results. Keys are ``TargetKey``, values are a tuple of
+            e-value and maximum tanimoto coefficient.
+        """
         return self.set_results_dict.get(mol_name, {})
 
     def target_result(self, target_id):
+        """Get results for target at all affinity levels.
+
+        To retrieve stats for molecule/target pairs, follow with
+        'mol_target_result'.
+
+        Parameters
+        ----------
+        target_id : str
+            Target id.
+
+        Returns
+        -------
+        target_results : set
+            Set of molecule names which hit to target.
+        """
         return self.target_results_dict.get(target_id, set([]))
 
     def mol_target_result(self, mol_name, target_id):
+        """Get results for mol/target pair at all affinities.
+
+        Parameters
+        ----------
+        mol_name : str
+            Name of molecule.
+        target_id : str
+            Target id.
+
+        Returns
+        -------
+        mol_target_results : tuple or None
+            Tuple of e-value and max tanimoto coefficient if a significant hit
+            was found between mol and target. Otherwise, None is returned.
+        """
         return self.set_results_dict.get(mol_name, {}).get(target_id, None)
 
     def close(self):
+        """Close library file."""
         if self.log:
             logging.info("Closing library.")
         self.core.close()
 
     def clear_results(self):
+        """Clear all results."""
         if self.log:
             logging.info("Clearing results from searcher.")
         self.set_results_dict.clear()
@@ -121,11 +223,29 @@ class SEASetSearcher(object):
 
 
 def sea_set_search(library_file, molecules_lists_dict, log=True):
-    """Get SEASetSearcher for molecule lists against a SEA library."""
+    """Get ``SEASetSearcher`` for molecule lists against a SEA library.
+
+    Parameters
+    ----------
+    library_file : str
+        Path to SEA library.
+    molecules_lists_dict : dict
+        Dict matching molecule name to list of native tuples.
+    log : bool, optional
+        Create log messages.
+
+    Returns
+    -------
+    searcher : SEASetSearcher
+        Instance of searcher used, containing results of batch search, for
+        convenience so results can be iterated or requested in any form.
+    """
     try:
         searcher = SEASetSearcher(library_file, log=log)
     except LockError:
-        os.remove("%s.lock" % library_file)
+        # SEA locks the library upon opening so it may only be opened once.
+        # However, sometimes the lock file is not deleted, so we ignore it.
+        os.remove("{!s}.lock".format(library_file))
         searcher = SEASetSearcher(library_file, log=log)
 
     searcher.batch_search(molecules_lists_dict)
