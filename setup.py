8f0067258b9e3eb055e9444501a9084c3cc88bab
Seth Axen <seth.axen@gmail.com>
2017-05-06 19:31:37 -0700

    Add setup files in prep for 1.0 release

---
+++ setup.py
@@ -0,0 +1,44 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requirements = [
+    'scipy>=0.18.0',
+    'numpy>=1.11.3',
+    'mmh3>=2.3.1',
+    'sdaxen_python_utilities>=0.1.3',
+]
+
+test_requirements = ['nose']
+
+classifiers = ['Programming Language :: Python',
+               'Programming Language :: Python :: 2 :: Only',
+               'Programming Language :: Python :: 2.7',
+               'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+               'Operating System :: OS Independent',
+               'Development Status :: 4 - Beta',
+               'Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'Topic :: Scientific/Engineering :: Chemistry',
+               'Topic :: Software Development :: Libraries :: Python Modules'
+               ]
+
+setup(
+    name='e3fp',
+    packages=['e3fp', 'e3fp.config', 'e3fp.conformer', 'e3fp.fingerprint',
+              'e3fp.test'],
+    version='1.0.0',
+    description='Molecular 3D fingerprinting',
+    keywords='e3fp 3d molecule fingerprint conformer',
+    author='Seth Axen',
+    author_email='seth.axen@gmail.com',
+    license='LGPLv3',
+    url='https://github.com/keiserlab/e3fp',
+    classifiers=classifiers,
+    download_url='https://github.com/keiserlab/e3fp/tarball/1.0',
+    install_requires=requirements,
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=test_requirements,
+)
