030dfe58e03de61aeea83b6f22097085d94751f5
Seth Axen <seth.axen@gmail.com>
2017-09-03 02:17:10 -0700

    Add another system path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,7 @@ from e3fp import __version__ as e3fp_version
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- General configuration ------------------------------------------------
 
