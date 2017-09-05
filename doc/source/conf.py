d175d277b2338fd9e8ccc8beaa4b2f38555d705d
Seth Axen <seth.axen@gmail.com>
2017-09-05 01:19:56 -0700

    Use RTD theme if available

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,12 @@ from mock import MagicMock
 sys.path.insert(0, os.path.abspath('../../'))
 from e3fp import __version__ as e3fp_version
 
+USE_DEFAULT_THEME = os.environ.get('READTHEDOCS') == 'True'
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    USE_DEFAULT_THEME = True
+
 
 class Mock(MagicMock):
     @classmethod
@@ -114,8 +120,10 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'default'
+if USE_DEFAULT_THEME:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
