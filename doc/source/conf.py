f9d51b8041285e6e77c06d8106f9d53cf3ba5490
Seth Axen <seth.axen@gmail.com>
2017-09-05 00:09:16 -0700

    Link scipy docs through inter sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -192,4 +192,5 @@ texinfo_documents = [
 intersphinx_mapping = {
     'python': ('http://docs.python.org/', None),
     'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+    'scipy': ('http://docs.scipy.org/doc/scipy/reference', None),
 }
