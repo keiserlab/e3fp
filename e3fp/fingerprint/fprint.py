6cb55ee8fa30d29713bc278c78de17be0aa6411e
Seth Axen <seth.axen@gmail.com>
2017-10-22 03:02:07 -0700

    Don't use dict in doctest snippet
    Dicts are ordered differently for each version of Python, so it is
    impossible for doctest to pass for all.

--- e3fp/fingerprint/fprint.py
+++ e3fp/fingerprint/fprint.py
@@ -762,12 +762,12 @@ class CountFingerprint(Fingerprint):
     [ 37 235 908  72 767 905 715 645 847 960 144 129 972 583 749 508 390 281
      178 276 254 357 914 468 907 252 490 668 925 398]
     >>> counts = dict(zip(indices, np.random.randint(1, 100, indices.shape[0])))
-    >>> print(counts)
-    {129: 62, 645: 14, 390: 61, ..., 508: 1, 254: 14, 767: 95}
+    >>> print(sorted(counts.items()))
+    [(37, 51), (72, 88), (129, 62), ..., (925, 50), (960, 8), (972, 23)]
     >>> f = fp.CountFingerprint(indices, counts=counts, bits=bits, level=0)
     >>> f_folded = f.fold(bits=32)
-    >>> print(f_folded.counts)
-    {0: 8, 1: 62, 5: 113, ..., 29: 50, 30: 14, 31: 95}
+    >>> print(sorted(f_folded.counts.items()))
+    [(0, 8), (1, 62), (5, 113), ..., (29, 50), (30, 14), (31, 95)]
     >>> print(f_folded.to_vector(sparse=False, dtype=int))
     [  8  62   0   0   0 113  61  58  88  97  71 228 111   2  58  10  64   0
       82   0 120   0   0   0   0  82   0   0  27  50  14  95]
@@ -776,8 +776,8 @@ class CountFingerprint(Fingerprint):
     >>> indices2 = np.random.randint(0, bits, 30)
     >>> counts2 = dict(zip(indices2, np.random.randint(1, 100, indices.shape[0])))
     >>> f_folded2 = fp.CountFingerprint.from_indices(indices2, counts=counts2, bits=bits).fold(bits=32)
-    >>> print(f_folded2.counts)
-    {0: 93, 2: 33, 3: 106, ..., 25: 129, 26: 89, 30: 53}
+    >>> print(sorted(f_folded2.counts.items()))
+    [(0, 93), (2, 33), (3, 106), ..., (25, 129), (26, 89), (30, 53)]
     >>> print(soergel(f_folded, f_folded2))
     0.174929463926
     """
