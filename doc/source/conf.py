5ffae6d77124da2198d69788d3df2f1937f27b48
Seth Axen <seth.axen@gmail.com>
2017-10-24 14:11:00 -0700

    Link out to Python 3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -206,8 +206,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('http://docs.python.org/', None),
-    'configparser': ('https://docs.python.org', None),
+    'python': ('https://docs.python.org/3/', None),
     'numpy': ('http://docs.scipy.org/doc/numpy/', None),
     'scipy': ('http://docs.scipy.org/doc/scipy/reference', None),
 }
