756df38698add19d66a58eefe0865a3b82d74da4
Seth Axen <seth.axen@gmail.com>
2017-10-23 17:53:17 -0700

    Use "autolink" default role
    By default “autolink” attempts to link to objects whenever possible,
    providing cleaner docstrings.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -109,6 +109,11 @@ release = version
 # Usually you set "language" from the command line for these cases.
 language = None
 
+# The reST default role (used for this markup: `text`) to use for all
+# documents. The autolink role functions as :obj: when the name referred can
+# be resolved to a Python object
+default_role = "autolink"
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
