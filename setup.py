d5760ba67567a8396778895c93c503c40e94479f
Seth Axen <seth.axen@gmail.com>
2020-02-24 20:30:31 -0800

    Run black on setup.py

--- setup.py
+++ setup.py
@@ -10,39 +10,40 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from e3fp import version
 
-ON_RTD = os.environ.get('READTHEDOCS') == 'True'
+ON_RTD = os.environ.get("READTHEDOCS") == "True"
 
 
 requirements = [
-    'scipy>=0.18.0',
-    'numpy>=1.11.3',
-    'mmh3>=2.3.1',
-    'cython>=0.25.2',
-    'sdaxen_python_utilities>=0.1.4',
+    "scipy>=0.18.0",
+    "numpy>=1.11.3",
+    "mmh3>=2.3.1",
+    "cython>=0.25.2",
+    "sdaxen_python_utilities>=0.1.4",
 ]
 if ON_RTD:  # ReadTheDocs can't handle C libraries
-    requirements = requirements[-1:] + ['mock']
+    requirements = requirements[-1:] + ["mock"]
 
-test_requirements = ['nose', 'mock']
+test_requirements = ["nose", "mock"]
 
-classifiers = ['Programming Language :: Python',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7',
-               'Programming Language :: Python :: 3.8',
-               'Programming Language :: Cython',
-               'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-               'Operating System :: OS Independent',
-               'Development Status :: 4 - Beta',
-               'Intended Audience :: Science/Research',
-               'Intended Audience :: Developers',
-               'Topic :: Scientific/Engineering :: Chemistry',
-               'Topic :: Software Development :: Libraries :: Python Modules'
-               ]
+classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Cython",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    "Operating System :: OS Independent",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "Topic :: Scientific/Engineering :: Chemistry",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
 
 
 def get_readme():
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         return f.read()
 
 
@@ -53,33 +54,43 @@ class LazyBuildExt(build_ext):
     def finalize_options(self):
         build_ext.finalize_options(self)
         import numpy
+
         self.include_dirs.append(numpy.get_include())
 
 
 cmdclass = {}
 ext_modules = []
-ext_modules += [Extension("e3fp.fingerprint.metrics._fast",
-                sources=["e3fp/fingerprint/metrics/_fast.pyx"])]
-cmdclass.update({'build_ext': LazyBuildExt})
+ext_modules += [
+    Extension(
+        "e3fp.fingerprint.metrics._fast",
+        sources=["e3fp/fingerprint/metrics/_fast.pyx"],
+    )
+]
+cmdclass.update({"build_ext": LazyBuildExt})
 
 setup(
-    name='e3fp',
-    packages=['e3fp', 'e3fp.config', 'e3fp.conformer', 'e3fp.fingerprint',
-              'e3fp.test'],
+    name="e3fp",
+    packages=[
+        "e3fp",
+        "e3fp.config",
+        "e3fp.conformer",
+        "e3fp.fingerprint",
+        "e3fp.test",
+    ],
     version=version,
-    description='Molecular 3D fingerprinting',
+    description="Molecular 3D fingerprinting",
     long_description=get_readme(),
-    keywords='e3fp 3d molecule fingerprint conformer',
-    author='Seth Axen',
-    author_email='seth.axen@gmail.com',
-    license='LGPLv3',
-    url='https://github.com/keiserlab/e3fp',
+    keywords="e3fp 3d molecule fingerprint conformer",
+    author="Seth Axen",
+    author_email="seth.axen@gmail.com",
+    license="LGPLv3",
+    url="https://github.com/keiserlab/e3fp",
     classifiers=classifiers,
-    download_url='https://github.com/keiserlab/e3fp/tarball/' + version,
+    download_url="https://github.com/keiserlab/e3fp/tarball/" + version,
     install_requires=requirements,
     include_package_data=True,
-    test_suite='nose.collector',
+    test_suite="nose.collector",
     tests_require=test_requirements,
     cmdclass=cmdclass,
-    ext_modules=ext_modules
+    ext_modules=ext_modules,
 )
