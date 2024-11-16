1db5d9ea515efade36aad517b0d9eec64f4043f7
Seth Axen <seth@sethaxen.com>
2024-11-17 00:37:21 +0100

    Revert "Require Python 3.9"
    This reverts commit 8257f0f3701a4f30fc43b14a59f57e6c5b8387bb.

--- setup.py
+++ setup.py
@@ -20,6 +20,9 @@ test_requirements = ["pytest", "mock"]
 
 classifiers = [
     "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
