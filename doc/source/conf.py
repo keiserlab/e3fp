99de095c35213d62a6edea2b2b8a07cfc5083806
Seth Axen <seth@sethaxen.com>
2024-11-21 14:48:27 +0100

    Specify language as english

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -88,7 +88,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents. The autolink role functions as :obj: when the name referred can
