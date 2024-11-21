b1b635a75cec92d2cca0bbdf5e7a904073d748ad
Seth Axen <seth@sethaxen.com>
2024-11-21 10:35:40 +0100

    Define version number in pyproject.toml

--- e3fp/__init__.py
+++ e3fp/__init__.py
@@ -1,5 +1,4 @@
+import importlib.metadata
 from .util import E3FPWarning, E3FPDeprecationWarning
 
-version_info = (1, 2, 6)
-version = ".".join(str(c) for c in version_info)
-__version__ = version
+__version__ = importlib.metadata.version("e3fp")
