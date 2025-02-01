e6d3ee715fb00e10cba3126c9d28d5d42982b0ed
j-adamczyk <jakub.adamczyk@placewise.com>
2025-02-01 12:22:25 +0100

    Allow Python 3.13

--- src/e3fp/__init__.py
+++ src/e3fp/__init__.py
@@ -1,4 +1,4 @@
 import importlib.metadata
 from .util import E3FPWarning, E3FPDeprecationWarning
 
-__version__ = importlib.metadata.version("e3fp")
+# __version__ = importlib.metadata.version("e3fp")
